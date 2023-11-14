2f98b75073f1a58cecbaf10a4824431667c39dd1
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-14 14:30:09 +0100

    Add helper script to copy build, run, exec scripts

---
+++ create-scripts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Error: Please specify a directory path as an argument."
+  exit 1
+fi
+
+ln build.sh "$1/build.sh"
+ln run.sh "$1/run.sh"
+ln exec.sh "$1/exec.sh"
