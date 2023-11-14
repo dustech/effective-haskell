187f646e278906530ae00a0e4c109c404fa93c26
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-14 12:16:45 +0100

    Add multiple let bindings
    Add short description of sho, print, and putStrLn

--- chapter1/creating-local-variables-using-let-bindings/Main.hs
+++ chapter1/creating-local-variables-using-let-bindings/Main.hs
@@ -23,4 +23,39 @@ extendedGreeting person =
       goodBye = makeGreeting "See you later" person
    in hello <> "\n" <> goodDay <> "\n" <> goodBye
 
-main = print $ extendedGreeting "George"
+main = putStrLn $ extendedGreeting "George"
+
+-- In Haskell, show, print, and putStrLn are three commonly used functions
+-- for output, but they serve slightly different purposes and behave in
+-- different ways:
+
+--   show:
+--       Type: Show a => a -> String
+--       Purpose: show takes a value of any type that is an instance of the Show
+--       type class and converts it to a string. It doesn't print anything to the
+--       screen. Instead, it produces a string representation of the value, which
+--       can then be printed using other functions.
+--       Example: show 123 produces the string "123".
+--       Usage: It's useful when you want to convert values into a string
+--       representation, for example, for concatenating with other strings
+--       or for later printing.
+
+--   putStrLn:
+--       Type: String -> IO ()
+--       Purpose: putStrLn takes a string and prints it to the console, followed
+--       by a new line. It's an IO function, meaning it produces an action which,
+--       when executed, will have a side effect
+--       (in this case, printing to the screen).
+--       Example: putStrLn "Hello, world!" prints "Hello, world!" followed
+--       by a new line.
+--       Usage: Used for printing strings to the console.
+
+--   print:
+--       Type: Show a => a -> IO ()
+--       Purpose: print is somewhat a combination of show and putStrLn. It takes
+--       a value of any type that is an instance of Show, converts it to a string
+--         using show, and then prints that string to the console followed
+--         by a new line. It's a convenient way to print values directly.
+--       Example: print 123 prints 123 to the console.
+--       Usage: Used when you want to print the string representation of a value
+--       directly to the console.
