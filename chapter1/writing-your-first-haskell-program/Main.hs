1abf0344a02733b86ce0a31541796093f2042580
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-14 08:29:41 +0100

    Add first program

---
+++ chapter1/writing-your-first-haskell-program/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+-- From your command line you can compile your program by typing ghc Main.
+-- you can skip the separate compilation step and just run your module by
+-- calling runhaskell Main.hs
+main = print "Hello, World!"
