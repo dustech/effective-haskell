4d795e67fd2e0b83928cfb4c3dd6eae1703b32ac
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-14 11:36:00 +0100

    Add some multiple let bindings

--- chapter1/creating-local-variables-using-let-bindings/Main.hs
+++ chapter1/creating-local-variables-using-let-bindings/Main.hs
@@ -6,8 +6,21 @@ makeGreeting salutation person =
 -- Next, update your makeGreeting method to create an
 -- intermediate value using a let binding.
 
+-- main = print $ makeGreeting "Hello" "George"
+
 makeGreeting' salutation person =
   let messageWithTrailingSpace = salutation <> " "
    in messageWithTrailingSpace <> person
 
-main = print $ makeGreeting' "Hello" "George"
+-- main = print $ makeGreeting' "Hello" "George"
+
+-- You aren’t limited to a single variable inside of a let binding;
+-- you can create as many different local variables as you want.
+
+extendedGreeting person =
+  let hello = makeGreeting "Hello" person
+      goodDay = makeGreeting "I hope you have a nice afternoon" person
+      goodBye = makeGreeting "See you later" person
+   in hello <> "\n" <> goodDay <> "\n" <> goodBye
+
+main = print $ extendedGreeting "George"
