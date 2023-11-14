2b33cd26095baccc49366d843bfa4b8bbe9593d9
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-14 10:54:06 +0100

    Add first let binding

--- chapter1/creating-local-variables-using-let-bindings/Main.hs
+++ chapter1/creating-local-variables-using-let-bindings/Main.hs
@@ -3,4 +3,11 @@ module Main where
 makeGreeting salutation person =
   salutation <> " " <> person
 
-main = print $ makeGreeting "Hello" "George"
+-- Next, update your makeGreeting method to create an
+-- intermediate value using a let binding.
+
+makeGreeting' salutation person =
+  let messageWithTrailingSpace = salutation <> " "
+   in messageWithTrailingSpace <> person
+
+main = print $ makeGreeting' "Hello" "George"
