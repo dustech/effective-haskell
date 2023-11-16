0574f914a1d20ca5b7b8dd024a3346ee7f0a9b7c
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-16 14:20:34 +0100

    Add infinite cycle list examples

--- chapter2/understanding-how-programs-are-evaluated/Main.hs
+++ chapter2/understanding-how-programs-are-evaluated/Main.hs
@@ -7,11 +7,30 @@ numbersStartingAt n =
   n : numbersStartingAt (n + 1)
 
 --  	numbersStartingAt 0 =
--- ​ 	  0 : <thunk>
+--  	  0 : <thunk>
 
 -- 0 : numbersStartingAt 1
 
 -- Which will give us:
--- ​ 	0 : 1 : <thunk>
+--  	0 : 1 : <thunk>
 
 main = print $ take 5 $ numbersStartingAt 0
+
+-- Instead of calculating the boundaries of an array, you can use the cycle function from Prelude to create an infinitely repeating list. Let’s look at an example of this by writing a function to convert radians to degrees. Our function will always return a number of degrees between 0 and 359 (we’ll only consider integer numbers of degrees), and instead of using modulo, we’ll index into a repeating list
+
+-- radsToDegrees :: Float -> Int
+radsToDegrees radians =
+  let degrees = cycle [0 .. 359]
+      converted = truncate $ (radians * 360) / (2 * pi)
+   in degrees !! converted
+
+-- For practice, let’s write our own version of cycle. Our version will be really cool, and we don’t want it to conflict with the existing function already named cycle, so let’s call ours epicCycle:
+
+epicCycle inputList =
+  cycleHelper inputList
+  where
+    cycleHelper [] = epicCycle inputList
+    cycleHelper (x : xs) = x : cycleHelper xs
+
+moreEpicCycle inputList =
+  inputList <> moreEpicCycle inputList
