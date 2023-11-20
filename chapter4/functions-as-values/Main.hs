ef3ecf3286c2db55a8a2007f67462c531cbd93bc
Stefano Cerruti <stefano.cerruti@outlook.com>
2023-11-20 18:03:18 +0100

    Add StringParser type with function

--- chapter4/functions-as-values/Main.hs
+++ chapter4/functions-as-values/Main.hs
@@ -1,3 +1,8 @@
 module Main where
 
 main = print ""
+
+-- More often than not, when you see data types that hold a single function,
+--  they’ll be records with a field that gives the function some useful name:
+
+data StringParser = StringParser {runStringParser :: String -> (String, String)}
