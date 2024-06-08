709065fc998ebbb4578790d268517eaf213a0970
dustech <stefano.cerruti@outlook.com>
2024-06-08 19:43:44 +0200

    Add creating modules files

--- chapter05/module-examples/src/HaskellBook/Examples/Introduction/CreatingModules.hs
+++ chapter05/module-examples/src/HaskellBook/Examples/Introduction/CreatingModules.hs
@@ -1,1 +1,50 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module HaskellBook.Examples.Introduction.CreatingModules where
+
+
+excitingMessage :: String -> String
+excitingMessage message =
+    "Exciting news: " <> message <> "!!!"
+
+newtype Name = Name { getName :: String }
+
+newtype Salutation = Salutation { getSalutation :: String}
+
+data GreetingMessage = GreetingMessage
+    {
+        greetingSalutation :: Salutation
+        , greetingTo       :: Name
+        , greetingFrom     :: [Name]
+    }
+
+defaultMessage :: GreetingMessage
+defaultMessage = GreetingMessage
+    {
+        greetingSalutation = Salutation "Hello"
+        , greetingTo = Name "Friend"
+        , greetingFrom = []
+    }
+
+formatMessage :: GreetingMessage -> String
+formatMessage GreetingMessage {..} =
+    greetingWithSuffix
+    where
+        basicGreeting =
+            getSalutation greetingSalutation <> " " <> getName greetingTo
+
+        greetingWithSuffix =
+            case greetingFrom of
+                [] -> basicGreeting <> "!"
+                [friend] ->
+                    basicGreeting <> ", from: " <> getName friend
+                [friendA, friendB] ->
+                    basicGreeting <> ", from: " <> getName friendA <> " and " <> getName friendB
+                friends ->
+                    basicGreeting <> ", from your friends: " <> formatFriendList friends
+
+        formatFriendList friends =
+            case friends of
+                [] -> ""
+                [friend] -> "and " <> getName friend
+                (friend:moreFriends) -> getName friend <> ", " <> formatFriendList moreFriends
