dae2fa93a57f1c9d3fdc3d47aaa8707c1d49499b
Stefano Cerruti <stefano.cerruti@outlook.com>
2025-05-12 11:11:17 +0000

    Cabal init

---
+++ chapter08/creating-a-pager/hcat/app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import qualified HCat (runHCat)
+
+main :: IO ()
+main = HCat.runHCat
---
+++ chapter08/creating-a-pager/hcat/src/HCat.hs
@@ -0,0 +1,6 @@
+module HCat (runHCat) where
+
+runHCat :: IO ()
+runHCat = 
+    putStrLn "\nrun HCat" >>
+    return ()
